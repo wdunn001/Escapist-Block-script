97cecfdf039b372e1142439d232effec3a495f0b
William Dunn <wdunn001@gmail.com>
2015-10-05 01:56:47 -0400

    init commit
    A script to block all escapist sites to prevent them from earning ad
    revenue from me

---
+++ escapistblock.bat
@@ -0,0 +1,47 @@
+REM This should be run with command prompt as an adminstrator.
+REM After disabling those, rename the file extension from .txt to .cmd
+REM Then run this script.
+
+REM First, block Windows from sending data about you to their servers.
+
+echo 0.0.0.0 www.break.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.smosh.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.mademan.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.clevver.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.youtube.com/shutupcartoons>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.escapistmagazine.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.screenjunkies.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.thegloss.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.youtube.com/aweme>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.gurl.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.cagepotato.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.teen.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.gamefront.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.damnyouautocorrect.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.blisstree.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.crushable.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.mommyish.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.everyjoe.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.shockwave.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 www.addictinggames.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 break.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 smosh.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 mademan.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 clevver.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 youtube.com/shutupcartoons>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 escapistmagazine.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 screenjunkies.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 thegloss.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 youtube.com/aweme>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 gurl.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 cagepotato.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 teen.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 gamefront.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 damnyouautocorrect.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 blisstree.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 crushable.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 mommyish.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 everyjoe.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 shockwave.com>>%windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 addictinggames.com>>%windir%\system32\drivers\etc\hosts
+
